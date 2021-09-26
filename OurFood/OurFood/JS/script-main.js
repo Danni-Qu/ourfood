2d472caf5dd2a9dafa8778422f68b157e8f3dd95
Danni Qu <71910882+Danni-Qu@users.noreply.github.com>
2021-09-26 03:05:38 -0400

    Update script-main.js

--- OurFood/OurFood/JS/script-main.js
+++ OurFood/OurFood/JS/script-main.js
@@ -15,3 +15,36 @@ function search_animal() {
         }
     }
 }
+
+
+const cursorTag = document.querySelector("div.cursors")
+  const balls = cursorTag.querySelectorAll("div")
+
+  let aimX = 0
+  let aimY = 0
+
+  balls.forEach((ball,index) => {
+    let currentX = 0
+    let currentY = 0
+  
+    let speed = 0.3 - index * 0.17
+
+    const animate = function(){
+        currentX += (aimX-currentX) * speed
+        currentY += (aimY-currentY) * speed
+        
+        ball.style.left = currentX + "px";
+        ball.style.top = currentY + "px";
+        
+        requestAnimationFrame(animate)
+        }
+        animate()
+
+  })
+
+  
+
+  document.addEventListener("mousemove", function(event){
+    aimX = event.pageX
+    aimY = event.pageY
+  })
